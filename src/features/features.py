59890a5dbdf5ebf6f2d086e95d4e02017de5cdf4
Ivan Dobryaev <dobryaev@MacBook-Air-Ivan.local>
2022-10-15 18:29:53 +0300

    add_early_wakeup func

--- src/features/features.py
+++ src/features/features.py
@@ -2,5 +2,11 @@ import pandas as pd
 
 
 def add_early_wakeup(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+
+    def _get__wakeup_time(time: str) -> int:
+
+        return int(time.split(':')[0])
+    
+    df['is_wake_up_early'] = df['Время пробуждения'].apply(lambda x: 1 if _get__wakeup_time(x) < 7 else 0)
+
     return df
